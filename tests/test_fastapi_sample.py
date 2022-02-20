e65a1373cae7e3df981ac64e92befd24338b9a92
INAJIMA Daisuke <inajima@gmail.com>
2022-02-20 14:02:55 +0900

    Add pytest-cov

--- tests/test_fastapi_sample.py
+++ tests/test_fastapi_sample.py
@@ -1,5 +1,15 @@
-from fastapi_sample import __version__
+from fastapi.testclient import TestClient
+
+from fastapi_sample import __version__, app
+
+client = TestClient(app)
 
 
 def test_version() -> None:
     assert __version__ == "0.1.0"
+
+
+def test_root() -> None:
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
